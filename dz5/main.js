7a7e1dabf4f65d34e4edd3e8d63a182fa977ebfb
Julia-Fedotovaa <146189747+Julia-Fedotovaa@users.noreply.github.com>
2024-01-11 02:38:59 +0300

    Update main.js

--- dz5/main.js
+++ dz5/main.js
@@ -1,189 +1,209 @@
-'use strict';
+// 'use strict';
 
 const API_KEY = '50d2199a-42dc-447d-81ed-d68a443b697e'
 const HOST = 'http://tasks-api.std-900.ist.mospolytech.ru/'
 
 let btn = document.querySelector('#create');
 
-function createItem(value){
-    const list = document.querySelector(`#${value.status}-list`);
-    const item = document.getElementById('task-template').cloneNode(true);
-   
-    item.querySelector('.task-name').textContent = value.name ? value.name : 'No name';
-    item.dataset.id=value.id;
-    item.dataset.status=value.status;
-    item.querySelector('.task-description').textContent = value.desc;
-    item.classList.remove('d-none');
-    list.append(item);
-       
+function createItem(value) {
+  const list = document.querySelector(`#${value.status}-list`);
+  const item = document.getElementById('task-template').cloneNode(true);
+  const moveDoneBtn = item.querySelector('.move-done')
+  moveDoneBtn.addEventListener('click', function(event) {
+    moveTaskToDone(value.id);
+  });
+
+  // const moveTodoBtn = item.querySelector('.move-todo');
+  // moveTodoBtn.addEventListener('click', function(event) {
+  //   moveTaskToTodo(value.id);
+  // });
+  item.querySelector('.task-name').textContent = value.name ? value.name : 'No name';
+  item.dataset.id = value.id;
+  item.dataset.status = value.status;
+  item.querySelector('.task-description').textContent = value.desc;
+  item.classList.remove('d-none');
+  list.append(item);
+
 }
 
-btn.addEventListener('click', function(event){
-    let modal = event.target.closest('.modal');
-    let name = modal.querySelector('#nameTask').value;
-    let description = modal.querySelector('#textTask').value;
-    let status = modal.querySelector('#select').value;
+btn.addEventListener('click', function (event) {
+  let modal = event.target.closest('.modal');
+  let name = modal.querySelector('#nameTask').value;
+  let description = modal.querySelector('#textTask').value;
+  let status = modal.querySelector('#select').value;
 
-    localStorage.setItem(`task-${localStorage.length+1}`,JSON.stringify({name, description, status}));
-    createTask(name, description, status);
+  localStorage.setItem(`task-${localStorage.length + 1}`, JSON.stringify({name, description, status}));
+  createTask(name, description, status);
 
-    //createItem({id:`task-${localStorage.length+1}`,name, description, status})
+  //createItem({id:`task-${localStorage.length+1}`,name, description, status})
 });
 
 const showShowModal = document.querySelector('#showModal');
-showShowModal.addEventListener('show.bs.modal',showModal)
+showShowModal.addEventListener('show.bs.modal', showModal)
 
-function showModal(event){
-    let task = event.relatedTarget.closest('#task-template');
-    let name = task.querySelector('.task-name').textContent;
-    let description = task.querySelector('.task-description').textContent;
-    event.target.querySelector('#showNameTask').value = name;
-    event.target.querySelector('#showTextTask').value = description;
+function showModal(event) {
+  let task = event.relatedTarget.closest('#task-template');
+  let name = task.querySelector('.task-name').textContent;
+  let description = task.querySelector('.task-description').textContent;
+  event.target.querySelector('#showNameTask').value = name;
+  event.target.querySelector('#showTextTask').value = description;
 };
 
 
 const showEditModal = document.querySelector('#editModal');
-showEditModal.addEventListener('show.bs.modal',editModal)
+showEditModal.addEventListener('show.bs.modal', editModal)
 
-function editModal(event){
-    let task = event.relatedTarget.closest('#task-template');
-    let name = task.querySelector('.task-name').textContent;
-    let description = task.querySelector('.task-description').textContent;
-    let id = task.dataset.id;
-    let status = task.dataset.status;
-    event.target.querySelector('#editNameTask').value = name;
-    event.target.querySelector('#editTextTask').value = description;
-    const btn = event.target.querySelector('#save');
-    btn.addEventListener('click', function(event) { 
-        name = document.getElementById('editNameTask').value
-        description = document.getElementById('editTextTask').value
-        //const item = JSON.parse(localStorage.getItem(id));
-        //item.name = name;
-        //item.description = description;
-        //localStorage.setItem(id, JSON.stringify(item));
-        editTask(id, name, description, status);
-        //task.querySelector('.task-name').textContent = item.name;
-        //task.querySelector('.task-description').textContent = item.desc;
-    });
+function editModal(event) {
+  let task = event.relatedTarget.closest('#task-template');
+  let name = task.querySelector('.task-name').textContent;
+  let description = task.querySelector('.task-description').textContent;
+  let id = task.dataset.id;
+  let status = task.dataset.status;
+  event.target.querySelector('#editNameTask').value = name;
+  event.target.querySelector('#editTextTask').value = description;
+  const btn = event.target.querySelector('#save');
+  btn.addEventListener('click', function (event) {
+    name = document.getElementById('editNameTask').value
+    description = document.getElementById('editTextTask').value
+    //const item = JSON.parse(localStorage.getItem(id));
+    //item.name = name;
+    //item.description = description;
+    //localStorage.setItem(id, JSON.stringify(item));
+    editTask(id, name, description, status);
+    //task.querySelector('.task-name').textContent = item.name;
+    //task.querySelector('.task-description').textContent = item.desc;
+  });
 }
 
 const showRemoveModal = document.querySelector('#removeModal');
-showRemoveModal.addEventListener('show.bs.modal',removeModal)
+showRemoveModal.addEventListener('show.bs.modal', removeModal)
 
-function removeModal(event){
-    let task = event.relatedTarget.closest('#task-template');
-    let name = task.querySelector('.task-name').textContent;
-    let id = task.dataset.id;
-    event.target.querySelector('#removeNameTask').textContent= name;
-    const btn = event.target.querySelector('#removeModalBtn');
-    btn.addEventListener('click', function(event) {
-        localStorage.removeItem(id);
-        deleteTask(id);
-        document.querySelector((`[data-id="${id}"]`)).remove();
-        
-    });
+function removeModal(event) {
+  let task = event.relatedTarget.closest('#task-template');
+  let name = task.querySelector('.task-name').textContent;
+  let id = task.dataset.id;
+  event.target.querySelector('#removeNameTask').textContent = name;
+  const btn = event.target.querySelector('#removeModalBtn');
+  btn.addEventListener('click', function (event) {
+    localStorage.removeItem(id);
+    deleteTask(id);
+    document.querySelector((`[data-id="${id}"]`)).remove();
+
+  });
 }
 
 function clearTasksLists() {
-    const lists = [...document.querySelectorAll('.list-group')]
+  const lists = [...document.querySelectorAll('.list-group')]
 
-    lists.map(list => [...list.children].map(
-        child => !child.classList.contains('d-none') ? child.remove() : null
-    ))
+  lists.map(list => [...list.children].map(
+    child => !child.classList.contains('d-none') ? child.remove() : null
+  ))
 }
 
-const showToDoneModal = document.getElementById('move-done');
-showToDoneModal.addEventListener('click',toDone)
 
-function toDone(event){
-    let task = event.relatedTarget.closest('#task-template');
-    let name = task.dataset.name;
-    let description = task.dataset.description;
-    let id = task.dataset.id;
-    let status = task.dataset.status;
-    btn.addEventListener('click', function(event) {
-        editTask(id, name, description, 'done');
-    });
+function moveTaskToDone(id) {
+  console.log(`MOVING ${id}`)
+  const taskElement = document.querySelector(`[data-id="${id}"]`);
+  const todoList = document.getElementById('to-do-list');
+  const doneList = document.getElementById('done-list');
+
+  todoList.removeChild(taskElement);
+  doneList.appendChild(taskElement);
+
+  editTask(id, taskElement.querySelector('.task-name').textContent, taskElement.querySelector('.task-description').textContent, 'done');
+  window.location.reload();
 }
 
+function moveTaskToTodo(id) {
+  console.log(`MOVING ${id}`)
+  const taskElement = document.querySelector(`[data-id="${id}"]`);
+  const todoList = document.getElementById('to-do-list');
+  const doneList = document.getElementById('done-list');
+
+  doneList.removeChild(taskElement);
+  todoList.appendChild(taskElement);
+
+  editTask(id, taskElement.querySelector('.task-name').textContent, taskElement.querySelector('.task-description').textContent, 'to-do');
+  window.location.reload();
+}
 
 async function deleteTask(id) {
-    console.log(`DELETING ${id}`)
-    const requestOptions = {
-        method: 'DELETE',
-        redirect: 'follow'
-    };
+  console.log(`DELETING ${id}`)
+  const requestOptions = {
+    method: 'DELETE',
+    redirect: 'follow'
+  };
 
-    fetch(
-        `${HOST}/api/tasks/${id}?api_key=${API_KEY}`,
-        requestOptions
-    )
-        .catch(error => console.log('error', error));
+  fetch(
+    `${HOST}/api/tasks/${id}?api_key=${API_KEY}`,
+    requestOptions
+  )
+    .catch(error => console.log('error', error));
 
-    getTasks();
+  getTasks();
 }
 
 async function createTask(name, description, status) {
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("desc", description);
-    formData.append("status", status);
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("desc", description);
+  formData.append("status", status);
 
-    const requestOptions = {
-        method: 'POST',
-        body: formData,
-        redirect: 'follow'
-    };
+  const requestOptions = {
+    method: 'POST',
+    body: formData,
+    redirect: 'follow'
+  };
 
-    fetch(
-        `${HOST}/api/tasks?api_key=${API_KEY}`,
-        requestOptions
-    )
-        .then(response => response.json())
-        .then(data => createItem(data))
-        .catch(error => console.log('error', error));
+  fetch(
+    `${HOST}/api/tasks?api_key=${API_KEY}`,
+    requestOptions
+  )
+    .then(response => response.json())
+    .then(data => createItem(data))
+    .catch(error => console.log('error', error));
 
-    getTasks();
+  getTasks();
 }
 
 async function editTask(id, name, description, status) {
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("desc", description);
-    formData.append("status", status);
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("desc", description);
+  formData.append("status", status);
 
-    const requestOptions = {
-        method: 'PUT',
-        body: formData,
-        redirect: 'follow'
-    };
+  const requestOptions = {
+    method: 'PUT',
+    body: formData,
+    redirect: 'follow'
+  };
 
-    fetch(
-        `${HOST}/api/tasks/${id}?api_key=${API_KEY}`,
-        requestOptions
-    )
-        .then(response => console.log(response.json()))
-        .catch(error => console.log('error', error));
+  fetch(
+    `${HOST}/api/tasks/${id}?api_key=${API_KEY}`,
+    requestOptions
+  )
+    .then(response => console.log(response.json()))
+    .catch(error => console.log('error', error));
 
-    getTasks();
+  getTasks();
 }
 
 async function getTasks() {
-    await fetch(`${HOST}/api/tasks?api_key=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-            clearTasksLists()
-            data.tasks.map(item=>createItem(item))
-        });
+  await fetch(`${HOST}/api/tasks?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      clearTasksLists()
+      data.tasks.map(item => createItem(item))
+    });
 }
 
-window.onload = function () { 
-    for (let i = 0; i < localStorage.length; ++i) {
-        let key = localStorage.key(i);
-        if (key.startsWith('task')) {
-            const value = JSON.parse(localStorage.getItem(key));
-            createItem({id:key,...value});
-        }
+window.onload = function () {
+  for (let i = 0; i < localStorage.length; ++i) {
+    let key = localStorage.key(i);
+    if (key.startsWith('task')) {
+      const value = JSON.parse(localStorage.getItem(key));
+      createItem({id: key, ...value});
     }
-    getTasks();
+  }
+  getTasks();
 };
